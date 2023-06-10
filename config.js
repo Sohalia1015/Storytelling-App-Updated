2f4855d6023235ab618044b3a6b64e6813bf82aa
Sohalia1015 <105957235+Sohalia1015@users.noreply.github.com>
2023-06-10 19:26:11 +1200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBMRnc8-Gh6GWuxZ-vtFN-rq4uloYTx6Mw",
+  authDomain: "storytelling-app-d7e0e.firebaseapp.com",
+  databaseURL: "https://storytelling-app-d7e0e-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-d7e0e",
+  storageBucket: "storytelling-app-d7e0e.appspot.com",
+  messagingSenderId: "583844012344",
+  appId: "1:583844012344:web:65a2bc5e33b980bd6fe1cd"
+};
